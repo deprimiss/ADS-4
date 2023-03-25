0980c4d0527c5d3f65617158b28522674e65fb40
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-03-25 23:42:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,7 +24,7 @@ int cbinsearch(int* arr, int size, int value, int n) {
     int count = 0;
     while (l < r - 1) {
         int sr = (l + r) / 2;
-        if (*(arr + n) * (arr + sr) == value) {
+        if (*(arr + n) + *(arr + sr) == value) {
             count++;
             for (int i = 1; *(arr + sr) == *(arr + (sr - i)); i++)
                 count++;
