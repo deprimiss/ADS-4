6914dc33173bb4c29fa006d81fb3eba0cde52f0a
deprimiss <125207464+deprimiss@users.noreply.github.com>
2023-03-25 23:40:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,49 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1;j < len;j++) {
+            if (*(arr + i) + *(arr + j) == value) count++;
+            }
+        }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    while (*(arr + (len - 1)) > value) len--;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (*(arr + i) + *(arr + j) == value) count++;
+        }
+    }
+    return count;
+}
+int cbinsearch(int* arr, int size, int value, int n) {
+    int l = n;
+    int r = size;
+    int count = 0;
+    while (l < r - 1) {
+        int sr = (l + r) / 2;
+        if (*(arr + n) * (arr + sr) == value) {
+            count++;
+            for (int i = 1; *(arr + sr) == *(arr + (sr - i)); i++)
+                count++;
+            for (int i = 0; *(arr + sr) == *(arr + (sr + i)); i++)
+                count++;
+        }
+        if (*(arr + n) + *(arr + sr) > value) {
+            r = sr;
+        }
+        else {
+            l = sr;
+        }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        count += cbinsearch(arr, len, value, i);
+    }
+    return count;
 }
